c1b474003e559b1f447cf33e4472bce5f68d6ae4
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2017-12-22 00:21:28 +0100

    Update Calculo_entradas.py

--- Calculo_entradas.py
+++ Calculo_entradas.py
@@ -1,31 +1,48 @@
 from tkinter import*
 from tkinter import messagebox
+
         
 def suma():
     try:
+        Salida=Text(ventana,width=20,height=1)
+        Salida.insert(INSERT,"")
+        Salida.place(x=10,y=218)
         solucion=numero1.get()+numero2.get()
         print(solucion)
+        Salida.insert(INSERT,solucion)
     except:
         messagebox.showwarning("ERROR","Operación no válida")
 
 def resta():
     try:
+        Salida=Text(ventana,width=20,height=1)
+        Salida.insert(INSERT,"")
+        Salida.place(x=10,y=218)
         solucion=numero1.get()-numero2.get()
         print(solucion)
+        Salida.insert(INSERT,solucion)
     except:
         messagebox.showwarning("ERROR","Operación no válida")
 
 def multi():
     try:
+        Salida=Text(ventana,width=20,height=1)
+        Salida.insert(INSERT,"")
+        Salida.place(x=10,y=218)
         solucion=numero1.get()*numero2.get()
         print(solucion)
+        Salida.insert(INSERT,solucion)
     except:
         messagebox.showwarning("ERROR","Operación no válida")
         
 def division():
     try:
+        Salida=Text(ventana,width=20,height=1)
+        Salida.insert(INSERT,"")
+        Salida.place(x=10,y=218)
         solucion=numero1.get()/numero2.get()
         print(solucion)
+        Salida.insert(INSERT,solucion)
     except:
         messagebox.showwarning("ERROR","Operación no válida")
 
@@ -42,5 +59,8 @@ boton=Button(ventana,text="Hacer suma",command=suma).place(x=10,y=100)
 boton2=Button(ventana,text="Hacer resta",command=resta).place(x=10,y=130)
 boton3=Button(ventana,text="Hacer multiplicacion",command=multi).place(x=10,y=160)
 boton4=Button(ventana,text="Hacer division",command=division).place(x=10,y=190)
+Salida=Text(ventana,width=20,height=1)
+Salida.insert(INSERT,"")
+Salida.place(x=10,y=218)
 ventana.mainloop()
 
